7efe033d1ea40b8098e4e00783f98486e5c57b26
Chris Rogers <chris@portchris.co.uk>
2020-04-05 09:33:21 +1000

    Ignore env and build without cache

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ docker network create nginx-proxy
 read -p "Use Docker cache? [Y/y]? " -n 1 -r
 echo # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]; then 
-	docker-compose build --no-cache --pull
-else
 	docker-compose build --pull
+else
+	docker-compose build --no-cache --pull
 fi
