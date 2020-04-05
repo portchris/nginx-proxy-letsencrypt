dd1e29291b295f3cbeaca55e0cf204fde4ad68bf
Chris Rogers <chris@portchris.co.uk>
2020-04-05 12:38:48 +1000

    Add no interaction to build

--- build.sh
+++ build.sh
@@ -2,10 +2,19 @@
 
 docker network create nginx-proxy
 
-read -p "Use Docker cache? [Y/y]? " -n 1 -r
-echo # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]; then 
-	docker-compose build --pull
-else
-	docker-compose build --no-cache --pull
+NO_CACHE="--no-cache"
+if [[ $* == *-n* ]]; then
+	NO_CACHE="--pull"
 fi
+
+if [[ $NO_CACHE == "--no-cache" ]]; then
+	read -p "Use Docker cache? [Y/y]? " -n 1 -r
+	echo # (optional) move to a new line
+	if [[ $REPLY =~ ^[Yy]$ ]]; then 
+		NO_CACHE="--pull"
+	else 
+		NO_CACHE="--no-cache"
+	fi
+fi
+
+docker-compose build $NO_CACHE
